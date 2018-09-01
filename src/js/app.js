757db430b2a81536064630a0810eca87f75afa67
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-09-01 00:17:54 -0400

    add real compnent files with proper structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,9 @@
 import 'MainStyle'
 import angular from 'angular'
 import mainController from './controllers/MainController'
+import welcomeComponent from './components/MainComponent';
 
 const app  = angular.module("app", []);
 app.controller("MainController", ['$scope', mainController]);
-app.component('test', {
-    template: 'Hello, {{$ctrl.user}}!',
-    controller: function GreetUserController() {
-      this.user = 'world';
-    }
-})
+app.component('test', mainComponent)
+app.component('welcomeComponent', welcomeComponent)
---
+++ src/js/components/welcome/index.js
@@ -0,0 +1,7 @@
+import template from './template.html'
+
+const welcomeComponent = {
+    template
+}
+
+export default welcomeComponent;
