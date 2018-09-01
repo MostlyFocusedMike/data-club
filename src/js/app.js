5027a0f9a0209e15d0a8b792eea3c26c71e49000
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-08-31 22:55:27 -0400

    add component in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,9 @@ import mainController from './controllers/MainController'
 
 const app  = angular.module("app", []);
 app.controller("MainController", ['$scope', mainController]);
+app.component('test', {
+    template: 'Hello, {{$ctrl.user}}!',
+    controller: function GreetUserController() {
+      this.user = 'world';
+    }
+})
