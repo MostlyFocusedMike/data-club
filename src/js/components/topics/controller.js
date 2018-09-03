8daf79625a8381c166d96db410c82c13fb35733f
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-09-03 17:41:55 -0400

    add show/hide topics button

--- src/js/components/topics/controller.js
+++ src/js/components/topics/controller.js
@@ -1,8 +1,23 @@
 import topics from '../../constants/topics'
+const SHOW = "Show Topics";
+const HIDE = "Hide Topics";
 
 const topicsCtrl = function() {
+    this.btnText = SHOW;
+    this.topicsVisible = false;
     this.topics = (meetingId) => {
         return topics.filter(topic => topic.meeting_id === meetingId)
+    }
+    this.getSum = function(total, obj) {
+        return parseInt(total) + parseInt(obj.duration);
+    }
+    this.totalHours = function(meetingId) {
+        return  this.topics(meetingId).reduce(this.getSum, 0) / 60;
+    }
+    this.hideTopics = function() {
+        this.topicsVisible = !this.topicsVisible;
+        this.btnText = this.topicsVisible ? SHOW : HIDE; 
+        console.log(this.topicsVisible)
     }
 }
 
